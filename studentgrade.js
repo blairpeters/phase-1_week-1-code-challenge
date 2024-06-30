944beb570326548917df7555b694a646f991a70e
Tony Blair Maina <blairpeters13@gmail.com>
2024-06-30 13:21:36 +0300

    completes lab

--- studentgrade.js
+++ studentgrade.js
@@ -2,20 +2,28 @@
  function studentGrade(marks) {
     if (marks > 79) {
       return 'A';
+
     } else if (marks >= 60) {
+
       return 'B';
+
     } else if (marks >= 50) {
+
       return 'C';
+
     } else if (marks >= 40) {
+
       return 'D';
+
     } else {
+
       return 'E';
     }
   }
   //generating the prompt
   
   function studentGradeGenerator() {
-    const marks = parseFloat(prompt('Enter the student marks (0-100):')); //floating poin number
+    const marks = parseFloat(prompt('Enter the student marks (0-100):')); //floating point number
   
     if (isNaN(marks) || marks < 0 || marks > 100) {
       return 'Invalid input. Please enter a valid marks between 0 and 100.';
